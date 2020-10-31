021495ad9b4b40414c8556439d98f8ec824f393a
Shiwei Yu <shiwei@Shiweis-MacBook-Pro.local>
2020-10-31 23:17:11 +0100

    modified test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,19 @@
 const cards = ['diamond', 'spade', 'heart', 'club'];
 
 // Write your code below
-let currentCard = 0;
-while (currentCard < cards.length) {
-  if (cards[currentCard] === 'spade') {
-    console.log(`index of spade is ${currentCard + 1}`);
+let i = 0;
+while (i < cards.length) {
+  if (cards[i] === 'spade') {
+    console.log(`index of spade is ${i + 1}`);
     break;
   } else {
-    currentCard++;
+    i++;
   }
+}
+
+// Write your code below
+let currentCard;
+while (currentCard !== 'spade') {
+  currentCard = cards[Math.floor(Math.random() * cards.length)];
+  console.log(currentCard);
 }
